94f13b41d618cdc4a11b4a1dabcf89c01cec9e13
fisher772 <kovachvladislav99@gmail.com>
2024-06-26 21:52:33 +0500

    v1.7

--- vpn_l2tp-ikev2/scripts/init.sh
+++ vpn_l2tp-ikev2/scripts/init.sh
@@ -12,7 +12,7 @@ PATH_IPSEC_CONF=/etc/ipsec.conf
 PATH_IPSEC_SECRETS=/etc/ipsec.secrets
 PATH_IPSEC_DOCKER_SECRETS=/etc/ipsec.d/ipsec.docker/ipsec.docker.secrets
 PATH_CHAP_SECRETS=/etc/ppp/chap-secrets
-PATH_LOG_CONF=
+PATH_LOG_CONF=/etc/rsyslog.conf
 PATH_DIR_CONF=
 PATH_
 
