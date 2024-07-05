a420b5db0da258f880fee1f79e8c05ea86b5e670
fisher772 <kovachvladislav99@gmail.com>
2024-07-05 16:09:20 +0500

    v3.3

--- vpn_l2tp-ikev2/make-vars.sh
+++ vpn_l2tp-ikev2/make-vars.sh
@@ -9,7 +9,7 @@ sed -i '/DOCKER_TOKEN:=/ s/=.*//' Makefile
 # set VERSION
 read -p "Enter value to replace VERSION with: " user_version
 if [[ -n "$user_version" ]]; then
-  sed -i "s|VERSION:=|VERSION:=$user_version|" Makefile
+  sed -i "s|VERSION:|VERSION:=$user_version|" Makefile
 else
   :
 fi
@@ -17,7 +17,7 @@ fi
 # set DOCKER_USER
 read -p "Enter value to replace DOCKER_USER with: " user_user
 if [[ -n "$user_user" ]]; then
-  sed -i "s|DOCKER_USER:=|DOCKER_USER:=$user_user|" Makefile
+  sed -i "s|DOCKER_USER:|DOCKER_USER:=$user_user|" Makefile
 else
   :
 fi
@@ -25,7 +25,7 @@ fi
 # set DOCKER_TOKEN
 read -p "Enter value to replace DOCKER_TOKEN with: " user_token
 if [[ -n "$user_token" ]]; then
-  sed -i "s|DOCKER_TOKEN:=|DOCKER_TOKEN:=$user_token|" Makefile
+  sed -i "s|DOCKER_TOKEN:|DOCKER_TOKEN:=$user_token|" Makefile
 else
   :
 fi
