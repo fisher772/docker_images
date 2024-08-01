2911a590e2db6231e38abc1302a74539aa5ba87b
fisher772 <kovachvladislav99@gmail.com>
2024-08-01 18:36:13 +0500

    v8.6

--- adguard/scripts/change_resolve.sh
+++ adguard/scripts/change_resolve.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 change_resolve() {
-  if [[ -f /etc/resolv.conf && grep -q "fisher adguard" /etc/resolv.conf ]]; then
+  if [[ -f /etc/resolv.conf ]] && grep -q "fisher adguard" /etc/resolv.conf; then
     mv -v /etc/resolv.conf /etc/resolv.conf.bak 2>/dev/null
     cat > /etc/resolv.conf <<EOF
 # fisher adguard
