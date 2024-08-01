2ba89b7c63bdca1168f446c714cd49b6684544fe
fisher772 <kovachvladislav99@gmail.com>
2024-08-01 18:39:05 +0500

    v8.3

---
+++ adguard/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [[ ! -z "${TZ}" ]]; then
+  cp /usr/share/zoneinfo/${TZ} /etc/localtime
+  echo ${TZ} >/etc/timezone
+fi
+
+replace_aliases () {
+  sed -i "s|LE_FQDN|${LE_FQDN}|g" /data/nginx/*.conf 2>/dev/null
+  sed -i "s|LE_FQDN|${LE_FQDN}|g" /data/nginx/stream/*.conf 2>/dev/null
+  sed -i "s|value-default|${COUNTAINER_ALIAS}|g" /data/nginx/*.conf 2>/dev/null
+  sed -i "s|value-default|${COUNTAINER_ALIAS}|g" /data/nginx/stream/*.conf 2>/dev/null
+  sed -i "s|SERVER_ALIAS|${SERVER_ALIAS}|g" /data/nginx/stream/*.conf 2>/dev/null
+}
+
+replace_aliases
