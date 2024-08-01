e332b64c1c3ef0ae2e431c48516ab62c01902430
fisher772 <kovachvladislav99@gmail.com>
2024-08-01 18:33:50 +0500

    v8.5

---
+++ adguard/scripts/change_resolve.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+change_resolve() {
+  if [[ -f /etc/resolv.conf && grep -q "fisher adguard" /etc/resolv.conf ]]; then
+    mv -v /etc/resolv.conf /etc/resolv.conf.bak 2>/dev/null
+    cat > /etc/resolv.conf <<EOF
+# fisher adguard
+[Resolve]
+DNS=127.0.0.1
+DNSStubListener=none
+EOF
+    systemctl restart systemd-resolved
+  else
+    :
+  fi
+}
+
+change_resolve
