a4f35e86dd990798295ab91c3df486645fb54504
fisher772 <kovachvladislav99@gmail.com>
2024-08-03 19:34:13 +0500

    v8.6

--- adguard/entrypoint.sh
+++ adguard/entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-if [[ ! -z "${TZ}" ]]; then
-  cp /usr/share/zoneinfo/${TZ} /etc/localtime
-  echo ${TZ} >/etc/timezone
-fi
-
 replace_aliases () {
   sed -i "s|LE_FQDN|${LE_FQDN}|g" /data/nginx/*.conf 2>/dev/null
   sed -i "s|LE_FQDN|${LE_FQDN}|g" /data/nginx/stream/*.conf 2>/dev/null
@@ -14,3 +9,5 @@ replace_aliases () {
 }
 
 replace_aliases
+
+exit 0
--- adguard/scripts/change_resolve.sh
+++ adguard/scripts/change_resolve.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 change_resolve() {
-  if [[ -f /etc/resolv.conf && ! -f /etc/resolv.conf.bak ]]; then
-    mkdir -p /etc/systemd/resolve.d 2>/dev/null
-    rm -f /etc/systemd/resolve.d/* 2>/dev/null
+  if [[ -f /etc/resolv.conf || true && ! -f /etc/resolv.conf.bak ]]; then
+    mkdir -p /etc/systemd/resolved.conf.d 2>/dev/null
+    rm -f /etc/systemd/resolved.conf.d/* 2>/dev/null
     mv -v /etc/resolv.conf /etc/resolv.conf.bak 2>/dev/null
-    cat > /etc/systemd/resolve.d/adguard.conf <<EOF
-# fisher adguard
+    cat > /etc/systemd/resolved.conf.d/adguard.conf <<EOF
+#fisher adguard
 [Resolve]
 DNS=127.0.0.1
-DNSStubListener=none
+DNSStubListener=no
 EOF
-    ln -s /etc/systemd/resolve.d/adguard.conf /etc/resolv.conf
+    ln -s /etc/systemd/resolved.conf.d/adguard.conf /etc/resolv.conf
     echo "Restart systemd-resolved"
     systemctl reload-or-restart systemd-resolved
   else
@@ -20,3 +20,5 @@ EOF
 }
 
 change_resolve
+
+exit 0
