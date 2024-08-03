d7215806c81c428585d16f2bc2de4500acb74dea
fisher772 <kovachvladislav99@gmail.com>
2024-08-03 23:47:40 +0500

    v9.7

--- adguard/entrypoint.sh
+++ adguard/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 replace_aliases () {
+  TR_SERVER_ALIAS=$(echo "$SERVER_ALIAS" | tr ',' ' ')
   sed -i "s|LE_FQDN|${LE_FQDN}|g" /data/nginx/*.conf 2>/dev/null
   sed -i "s|LE_FQDN|${LE_FQDN}|g" /data/nginx/stream/*.conf 2>/dev/null
   sed -i "s|value-default|${COUNTAINER_ALIAS}|g" /data/nginx/*.conf 2>/dev/null
   sed -i "s|value-default|${COUNTAINER_ALIAS}|g" /data/nginx/stream/*.conf 2>/dev/null
-  sed -i "s|SERVER_ALIAS|${SERVER_ALIAS}|g" /data/nginx/stream/*.conf 2>/dev/null
+  sed -i "s|SERVER_ALIAS|${TR_SERVER_ALIAS}|g" /data/nginx/stream/*.conf 2>/dev/null
 }
 
 replace_aliases
