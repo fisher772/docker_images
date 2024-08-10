ad0966506f00e25fd16c5f055e480cbd11be05a1
fisher772 <kovachvladislav99@gmail.com>
2024-08-10 10:57:24 +0500

    v10.3

--- 3x-ui/DockerEntrypoint.sh
+++ 3x-ui/DockerEntrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Set address for Nginx
+sed -i "s|LE_FQDN|${LE_FQDN}|g" /data/nginx/*.conf 2>/dev/null
+sed -i "s|LE_FQDN|${LE_FQDN}|g" /data/nginx/stream/*.conf 2>/dev/null
+
 # Start fail2ban
 fail2ban-client -x start
 
