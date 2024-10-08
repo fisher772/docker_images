903eeb3b54a718511abf1053498e8a090759d64d
fisher772 <kovachvladislav99@gmail.com>
2024-10-08 22:08:56 +0500

    v11.3

--- 3x-ui/DockerEntrypoint.sh
+++ 3x-ui/DockerEntrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
 # Set address for Nginx
-sed -i "s|LE_FQDN|${LE_FQDN}|g" /data/nginx/*.conf 2>/dev/null
-sed -i "s|LE_FQDN|${LE_FQDN}|g" /data/nginx/stream/*.conf 2>/dev/null
+replace_aliases () {
+  sed -i "s|LE_FQDN|${LE_FQDN}|g" /data/nginx/conf/*.conf 2>/dev/null
+  sed -i "s|LE_FQDN|${LE_FQDN}|g" /data/nginx/stream/*.conf 2>/dev/null
+  sed -i "s|value-default|${CONTAINER_ALIAS}|g" /data/nginx/conf/*.conf 2>/dev/null
+  sed -i "s|value-default|${CONTAINER_ALIAS}|g" /data/nginx/stream/*.conf 2>/dev/null
+  sed -i "s|SERVER_ALIAS|${SERVER_ALIAS}|g" /data/nginx/stream/*.conf 2>/dev/null
+}
+
+replace_aliases
 
 # Start fail2ban
 fail2ban-client -x start
